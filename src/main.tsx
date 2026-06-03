471de6f7186af3c0fc7c400e62b4ba02f34aaa18
Osito Kale <advalconcepts@gmail.com>
2026-06-03 17:12:56 +0300

    fix(bootstrap): RouterProvider bootstrap, remove Bolt scaffold and react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { RouterProvider } from '@tanstack/react-router'
+import { getRouter } from './router'
 import './index.css'
-import App from './App.tsx'
+
+const router = getRouter()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
