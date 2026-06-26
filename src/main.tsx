4dc8b3cb6a5dfb4a71a49cef0b83737f7d541f35
Osito Kale <advalconcepts@gmail.com>
2026-06-26 18:22:37 +0300

    Added project-bolt-github-xsdagmjn.zip

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from '@tanstack/react-router'
-import { getRouter } from './router'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-
-const router = getRouter()
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
